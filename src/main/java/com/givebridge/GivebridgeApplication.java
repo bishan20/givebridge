81fa9487f392f93d276da6b94eff5226688598ea
bishan20 <bishanrasaili20@gmail.com>
2026-04-01 14:18:21 -0400

    feat: initial Spring Boot Maven project setup

---
+++ src/main/java/com/givebridge/GivebridgeApplication.java
@@ -0,0 +1,13 @@
+package com.givebridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivebridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivebridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/givebridge/GivebridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.givebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivebridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
